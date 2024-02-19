e812d184696a5f6e64cca0d777c088e0423bcbdf
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:14:34 +0300

    Add files via upload

---
+++ a15/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fun(int a[],int n)
+{
+    int x;
+    if(n==1)
+    {
+        return a[0];
+    }
+    else
+    {
+        x=fun(a,n-1);
+
+    }
+    if (x>a[n-1])
+    {
+        return x;
+
+    }
+    else
+    {
+        return a[n-1];
+    }
+}
+
+int main()
+
+
+{
+    int arr[]={12,10,300,50,100};
+    int a=5;
+    printf("%d",fun(arr,a));
+    getchar();
+
+}
