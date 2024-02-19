2c8bfb9e0327e1ed5ff25953e3354d096efcb645
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:09:51 +0300

    Add files via upload

---
+++ a3/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int matris[10][10];
+
+
+   int i,j;
+
+
+   for(i=0;i<10;i++)
+   {
+       for(j=0;j<10;j++)
+       {
+           if(i==j)
+           {
+               matris[i][j]=1;
+           }
+           else
+           {
+               matris [i][j]=0;
+           }
+       }
+   }
+   for(i=0;i<10;i++)
+   {
+       for(j=0;j<10;j++){
+       printf("%d ",matris[i][j]);
+       }printf("\n");
+
+
+       }
+}
