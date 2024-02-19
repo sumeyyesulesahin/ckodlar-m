21dc99b25a22ebbc17ee879c9c9bfc5c338fed53
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:35:58 +0300

    Add files via upload

---
+++ k1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+int girileceknot;
+printf("ogrencinin notunu giriniz");
+scanf("%d",&girileceknot);
+if(girileceknot>=50){
+printf("bu ogrenci sinifi gecmistir");
+}
+else{
+ printf("bu ogrenci sinifi gecememistir");
+}
+
+
+
+}
