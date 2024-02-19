39ec6991f443c97198b63af23b393a55ef7d7ff3
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:33:34 +0300

    Add files via upload

---
+++ d2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi;
+        printf("lutfen bir sayi giriniz");
+        scanf("%d",&sayi);
+
+    if(5>sayi>2)
+        printf("evet");
+    else
+        printf("hayir");
+    return 0;
+}
