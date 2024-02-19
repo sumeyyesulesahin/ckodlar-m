44e64389ab9eb29d7603e03c8f4a6c17d5fae042
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:31:29 +0300

    Add files via upload

---
+++ c1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("lutfen bir sayi giriniz");
+  int a;
+  scanf("%d",&a);
+
+
+  if(a>0)
+  {
+      printf("sayi 0 dan buyuktur");
+  }
+   else if(a=0)
+   {
+       printf("sayi 0 a esittir");
+
+   }
+   else
+   {
+       printf("sayi sifirdan kucuktur");
+   }
+
+
+
+
+}
