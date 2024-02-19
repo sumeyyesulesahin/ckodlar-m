4227088bab55b4812017d047cac829974036a29c
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:33:52 +0300

    Add files via upload

---
+++ d3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi1,sayi2,toplam;
+        printf("lutfen ilk sayiyi giriniz:");
+        scanf("%d",&sayi1);
+        printf("lutfen ikinci sayiyi giriniz:");
+        scanf("%d",&sayi2);
+        toplam=sayi1+sayi2;
+        printf("%d",toplam);
+
+            return 0;
+}
