263de49b1b36748adc4f5051b8989443e229ad3e
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2024-02-19 15:40:56 +0300

    Add files via upload

---
+++ k16/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int yas;
+        printf("lutfen bir sayi giriniz");
+        scanf("%d",&yas);
+    if(yas>=18)
+    {
+     printf("oy kullanabilir");
+    }
+     else
+    printf("oy kullanamaz");
+            return 0;
+}
